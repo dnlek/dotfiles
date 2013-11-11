25912e305baa98318844b4044221590e417162f6
Daniel Wagner <dnl.wagner@gmail.com>
2013-11-11 19:16:54 +0100

    added git config

--- setup.sh
+++ setup.sh
@@ -16,6 +16,11 @@ ln -f -s ${CUR_DIR}/vimrc ${HOME}/.vimrc
 echo "ln ${HOME}/.tmux.conf"
 ln -f -s ${CUR_DIR}/tmux.conf ${HOME}/.tmux.conf
 
+echo "ln ${HOME}/{.gitconfig,.gitignore}"
+ln -f -s ${CUR_DIR}/gitconfig ${HOME}/.gitconfig
+ln -f -s ${CUR_DIR}/gitignore ${HOME}/.gitignore
+
+
 # install fonts
 echo "install fonts in ${HOME}/.fonts"
 cp fonts/* ${HOME}/.fonts/
